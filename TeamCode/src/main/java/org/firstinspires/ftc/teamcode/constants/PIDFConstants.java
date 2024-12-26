19fba381a3989094d239045e73d0a5c80f82b1f4
0verkil <112268976+0verkil@users.noreply.github.com>
2024-12-26 14:53:36 -0800

    feat: Global Constants

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/constants/PIDFConstants.java
@@ -0,0 +1,17 @@
+package org.firstinspires.ftc.teamcode.constants;
+
+import com.acmerobotics.dashboard.config.Config;
+
+/**
+ * This class is a global constants class to ensure that your constants from tuning will save when testing.
+ */
+@Config
+public class PIDFConstants {
+
+    public static double p = 0;
+
+    public static double d = 0;
+
+    public static double f = 0;
+
+}
---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/constants/SquIDConstants.java
@@ -0,0 +1,13 @@
+package org.firstinspires.ftc.teamcode.constants;
+
+import com.acmerobotics.dashboard.config.Config;
+
+/**
+ * This class is a global constants class to ensure that your constants from tuning will save when testing.
+ */
+@Config
+public class SquIDConstants {
+
+    public static double p = 0;
+
+}
